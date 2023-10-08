89f5319eca049705bdd7f1926257a3603f43d136
Ellis Rugazoora <ellisrgrz@gmail.com>
2023-10-07 17:52:35 -0700

    added music notes inputs

--- src/App.js
+++ src/App.js
@@ -4,40 +4,56 @@ import { hello_world, placeholder } from './functions';
 import Testcomponent from './Testcomponent';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { isVisible } from '@testing-library/user-event/dist/utils';
+import Assign from './Assign';
 var mapping_object = {};
 
 function App() {
   const [number, SetNumber] = useState(parseInt(localStorage.getItem('number')))
-  const [arr, SetArray] = useState(0)
+  const [array, SetArray] = useState([1])
   var output = [];
-  /*
-  window.addEventListener('keydown', (e) => {
-    //SetArray(arr + 1)
-    output.push({note: e.key, spacing: 1})
-    //console.log(output)
-  });*/
-  useEffect(() => {
+  
+  useEffect(() => {//this is for storage
     localStorage.setItem('number', number.toString());
   }, [number]);
   function print_storage(){
     SetNumber(number + 1)
     console.log("the stored number: " + localStorage.getItem('number'))
-  }/*
-  useEffect(() => {
-    
+  }
+  const [vis, SetVis] = useState("true")
+  
+  function handleKeyDown(event) {
+    console.log('Key pressed and arr:', event.key, array);
+    //let cheese = arr.push(1)
+    SetArray(x => [...x,event.key])
+    //SetArray(function(input){input.push(1); return input})
+    /*
+    SetArray((x) => {
+      var n = x.push(event.key)
+      return x
+      //return event.key
+    })*/
+  }
 
-  }, [])*/
+  useEffect(() => {
+    // Add the event listener when the component mounts
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <p>
           The music app. {placeholder("text", "Objective for music app")}
         </p>
-        {output}
-        {arr}
+        The value of array: {array}
+        {/*<button onClick={() => {SetArray(x => 9)}}>increment array</button>*/}
         <img src={logo} className="App-logo" alt="logo" />
-        <div hidden="true" className='capability tests'>
+        <button onClick={function(){let obj = {"": "true", "true": ""}; SetVis(obj[vis])}}>Show/hide: Test</button>
+        <div hidden={vis} className='capability tests'> {/*Add a button that toggles visibility*/}
           {placeholder("Text", "The interface of an application that will achieve the objective that corresponds to this app")}
           <button onClick={hello_world}>hello world</button>
           <button onClick={print_storage}>test local storage</button>
@@ -50,20 +66,13 @@ function App() {
           Proofs of concepts <br></br>
           {placeholder("button", "User input")}
           {output}
-          {placeholder("Visual", "inputdependent visualization")}
+          {placeholder("Visual", "inputdependent visualization")}<br></br>
           {placeholder("input (text or number)", "configure the model")}<br></br>
           {placeholder("input (text or number)", "configure success metric")}<br></br>
+          {placeholder("input (text or number)", "configure level of assistance")}<br></br>
           {placeholder("text", "object representing the input-dependent music-representing model that will be evaluated")}
+          <Assign placeholder="Placeholder fxn input" map="testmap"/>
         </div>
-        
-        {/*<a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reaction
-        </a>*/}
       </header>
     </div>
   );
---
+++ src/Assign.js
@@ -0,0 +1,11 @@
+function Assign(props){
+    return (
+        <div /*hidden={props.visible}*/>
+            <p>
+                Assign test: {props.placeholder} based on {props.map}
+            </p>
+        </div>
+    )
+}
+
+export default Assign;
